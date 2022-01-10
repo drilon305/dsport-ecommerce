76a30779e98c950c8df664812f557a0b1aacf6ea
Drilon <navycrack1@gmail.com>
2022-01-11 00:30:01 +0100

    styling sign up page

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
+import clsx from 'clsx'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import TextField from  '@material-ui/core/TextField'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import { makeStyles } from '@material-ui/core/styles'
+
+import Fields from './Fields'
+import { EmailPassword } from './Login'
 
 import addUserIcon from '../../images/add-user.svg'
 import nameAdornment from '../../images/name-adornment.svg'
@@ -37,67 +41,87 @@ const useStyles = makeStyles(theme => ({
         height: '4rem',
         width: '4rem'
       },
+      visibleIcon: {
+        padding: 0,
+      },
+      emailAdornment: {
+        width: 22,
+        height: 17,
+        marginBottom: 10,
+    },
+    removeBtnMargin: {
+        marginTop: 0,
+    },
 }))
 
 export default function SignUp({ steps, setSelectedStep}) {
     const classes = useStyles()
-    const [name, setName] = useState('')
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+      })
+        const [errors, setErrors] = useState({})
+        const [visible, setVisible] = useState(false)
     const [info, setInfo] = useState(false)
 
     const handleNavigate = direction => {
         if(direction === 'forward') {
             setInfo(true)
         } else {
-            const login = steps.find(step => step.label === 'Login');
-
-            setSelectedStep(steps.indexOf(login))
+            if(info) {
+                setInfo(false)
+            } else {
+                const login = steps.find(step => step.label === 'Login');
+                setSelectedStep(steps.indexOf(login))
+            }
+           
         }
     }
+
+    const nameField = { name: {
+        helperText: 'you must enter a name' ,
+        placeholder: 'Name',
+        startAdornment: (
+            <img src={nameAdornment} alt='name' />
+
+        )  
+    }}
+
+
+    const fields = info ? EmailPassword(classes, false, false, visible, setVisible) : nameField
 
     return (
         <>
         <Grid item>
             <img src={addUserIcon} alt='new user' className={classes.addUserIcon} />
         </Grid>
-        <Grid item>
-        <TextField
-          value={name}
-          onChange={e => {
-           setName(e.target.value)
-          }}
-          placeholder='Name'
-          classes={{ root: classes.textField }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position='start'>
-                <span className={classes.emailAdornment}>
-                  <img src={nameAdornment} alt='name' />
-                </span>
-              </InputAdornment>
-            ),
-            classes: { input: classes.input }
-          }}
-        />
-        </Grid>
-        <Grid item>
-            <Button variant='contained' color='secondary' classes={{root: classes.fbSignUp}}>
-                <Typography variant='h5' classes={{root: classes.fbText}}>
-                    Sign Up with Facebook
-                </Typography>
-            </Button>
-        </Grid>
-        <Grid item container justifyContent='space-between'>
+            <Fields fields={fields} errors={errors} setErrors={setErrors} values={values} setValues={setValues} />
             <Grid item>
-                <IconButton onClick={() => handleNavigate('backward')}>
-            <img src={backward} alt='back to login' className={classes.navigation} />            
-             </IconButton>
+                <Button variant='contained' color='secondary' classes={{
+                    root: clsx(classes.fbSignUp, {
+                        [classes.removeBtnMargin]: info
+                    })
+                }}>
+                    <Typography variant='h5' classes={{ root: classes.fbText }}>
+                        Sign Up{info ? '' : ' with Facebook'}
+                    </Typography>
+                </Button>
             </Grid>
-            <Grid item>
-                <IconButton onClick={() => handleNavigate('forward')}>
-                <img src={forward} alt='continue registration' className={classes.navigation} />      
-                </IconButton>
+            <Grid item container justifyContent='space-between'>
+                <Grid item>
+                    <IconButton onClick={() => handleNavigate('backward')}>
+                        <img src={backward} alt='back to login' className={classes.navigation} />
+                    </IconButton>
+                </Grid>
+                {info ? null : (
+                    <Grid item>
+                    <IconButton onClick={() => handleNavigate('forward')}>
+                        <img src={forward} alt='continue registration' className={classes.navigation} />
+                    </IconButton>
+                </Grid>
+                )}
+                
             </Grid>
-        </Grid>
         </>
     )
 }

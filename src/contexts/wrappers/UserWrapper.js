89d011c73f46d7ccc3df0d90941306aedaa9ed6a
Drilon <navycrack1@gmail.com>
2022-01-17 20:41:50 +0100

    handling token expiration

--- src/contexts/wrappers/UserWrapper.js
+++ src/contexts/wrappers/UserWrapper.js
@@ -1,5 +1,6 @@
-import React, { useReducer, createContext} from 'react'
+import React, { useEffect, useReducer, createContext} from 'react'
 import userReducer from '../reducers/user-reducer'
+import axios from 'axios'
 import { setUser } from '../actions'
 
 export const UserContext = createContext()
@@ -9,6 +10,22 @@ export function UserWrapper({ children }) {
     const defaultUser = { username: 'Guest'}
     const storedUser = JSON.parse(localStorage.getItem('user'))
     const [user, dispatchUser] = useReducer(userReducer, storedUser || defaultUser);
+
+    useEffect(() => {
+        if(storedUser) {
+            setTimeout(() => {
+            axios.get(process.env.GATSBY_STRAPI_URL + '/users/me', {
+                headers: {
+                    Authorization: `Bearer ${storedUser.jwt}`
+                }
+            }).then(res => {
+                dispatchUser(setUser({ ...res.data, jwt: storedUser.jwt}))
+            }).catch(error => {
+                dispatchUser(setUser(defaultUser))
+            })
+            }, 3000)
+        }
+    }, [])
 
     return (
         <UserProvider value={{ user, dispatchUser, defaultUser}}>

b65720bfe560a4605600f15713ead34e02679f16
Drilon <navycrack1@gmail.com>
2022-02-11 19:34:18 +0100

    get city from zip code

--- src/components/settings/Location.js
+++ src/components/settings/Location.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
 import Chip from '@material-ui/core/Chip'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Fields from '../auth/Fields'
 import Slots from './Slots'
+
+import { FeedbackContext } from '../../contexts'
+import { setSnackbar } from '../../contexts/actions'
 
 import locationIcon from '../../images/location.svg'
 import streetAdornment from '../../images/street-adornment.svg'
@@ -45,6 +50,26 @@ export default function Location({
   setErrors,
 }) {
   const classes = useStyles()
+  const [loading, setLoading] = useState(false)
+  const { dispatchFeedback } = useContext(FeedbackContext)
+
+  const getLocation = () => {
+    setLoading(true)
+
+    axios.get(
+      `https://data.opendatasoft.com/api/records/1.0/search/?dataset=geonames-postal-code%40public&rows=1&facet=country_code&facet=admin_name1&facet=place_name&facet=postal_code&refine.country_code=US&refine.postal_code=${values.zip}`
+    ).then(response => {
+      setLoading(false)
+
+      const { place_name, admin_name1} = response.data.records[0].fields
+
+      setValues({ ...values, city: place_name, state: admin_name1})
+    }).catch(error => {
+      setLoading(false)
+      console.error(error)
+      dispatchFeedback(setSnackbar({status: 'error', message: 'There was a problem with your zipcode, please try again.'}))
+    })
+  }
 
   useEffect(() => {
     setValues(user.locations[slot])
@@ -56,6 +81,14 @@ export default function Location({
     )
 
     setChangesMade(changed)
+
+    if(values.zip.length === 5) {
+      if(values.city) return
+
+      getLocation()
+    } else if (values.zip.length < 5 && values.city) {
+      setValues({...values, city: '', state: ''})
+    }
   }, [values])
 
   const fields = {
@@ -106,11 +139,14 @@ export default function Location({
         />
       </Grid>
       <Grid item classes={{ root: classes.chipWrapper }}>
-        <Chip
+        {loading ?  <CircularProgress  color='secondary' /> : (
+          <Chip
           label={
             values.city ? `${values.city}, ${values.state}` : "City, State"
           }
         />
+        )}
+        
       </Grid>
       <Grid item container classes={{ root: classes.slotContainer }}>
         <Slots slot={slot} setSlot={setSlot} />

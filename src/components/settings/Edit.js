1d4c6329860c39d068f07c920e6c498382475acc
Drilon <navycrack1@gmail.com>
2022-03-16 23:51:30 +0100

    cart context actions and reducer

--- src/components/settings/Edit.js
+++ src/components/settings/Edit.js
@@ -44,7 +44,7 @@ export default function Edit({
   const classes = useStyles()
   const { dispatchFeedback } = useContext(FeedbackContext)
   const [loading, setLoading] = useState(false)
-  const [dialogOpen, setDialogOpen] = useState(true)
+  const [dialogOpen, setDialogOpen] = useState(false)
 
   const handleEdit = () => {
     if(edit && isError) {
--- src/contexts/actions/index.js
+++ src/contexts/actions/index.js
@@ -1,3 +1,5 @@
 export { setUser } from './user-action'
 
 export { setSnackbar } from './feedback-actions'
+
+export { addToCart, removeFromCart, clearCart } from './'

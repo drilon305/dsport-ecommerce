a0094d7527a4e1c08fefcc93c0264b78932bafb6
Drilon <navycrack1@gmail.com>
2022-03-21 00:50:34 +0100

    completed qtybutton for the cart

--- src/components/cart/Item.js
+++ src/components/cart/Item.js
@@ -80,6 +80,7 @@ export default function Item({ item }) {
               selectedVariant={0}
               variants={[item.variant]}
               stock={[{ qty: item.stock }]}
+              isCart
             />
           </Grid>
         </Grid>

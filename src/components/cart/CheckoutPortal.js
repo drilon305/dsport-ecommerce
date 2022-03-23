8185c3cc37e4cae6723007801f3c48508cfbdb43
Drilon <navycrack1@gmail.com>
2022-03-24 00:56:53 +0100

    checkout address

--- src/components/cart/CheckoutPortal.js
+++ src/components/cart/CheckoutPortal.js
@@ -5,12 +5,21 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import CheckoutNavigation from './CheckoutNavigation'
 import Details from '../settings/Details'
+import Location from '../settings/Location'
 
 const useStyles = makeStyles(theme => ({
     stepContainer: {    
         width: '40rem',
         height: '25rem',
         backgroundColor: theme.palette.primary.main,
+    },
+    "@global": {
+      ".MuiInput-underline:before, .MuiInput-underline:hover:not(.Mui-disabled):before": {
+        borderBottom: "2px solid #fff",
+      },
+      ".MuiInput-underline:after": {
+        borderBottom: `2px solid ${theme.palette.secondary.main}`,
+      },
     },
 }))
 
@@ -19,13 +28,47 @@ export default function CheckoutPortal({ user }) {
     const [selectedStep, setSelectedStep] = useState(0)
     const [detailValues, setDetailValues] = useState({name: '', email: '', phone: ''})
     const [detailSlot, setDetailSlot] = useState(0)
+    const [locationValues, setLocationValues] = useState({ state: '', zip: '', city: '', state: ''})
+    const [locationSlot, setLocationSlot] = useState(0)
+    const [locationBilling, setLocationBilling] = useState(false)
     const [detailBilling, setDetailBilling] = useState(false)
     const [errors, setErrors] = useState({})
 
     const steps = [
-      { title: "Contact Info", component: <Details user={user} values={detailValues} setValues={setDetailValues} 
-     slot={detailSlot} setSlot={setDetailSlot} errors={errors} setErrors={setErrors} checkout billing={detailBilling} setBilling={setDetailBilling} /> },
-      { title: "Address" },
+      {
+        title: "Contact Info",
+        component: (
+          <Details
+            user={user}
+            values={detailValues}
+            setValues={setDetailValues}
+            slot={detailSlot}
+            setSlot={setDetailSlot}
+            errors={errors}
+            setErrors={setErrors}
+            checkout
+            billing={detailBilling}
+            setBilling={setDetailBilling}
+          />
+        ),
+      },
+      {
+        title: "Address",
+        component: (
+          <Location
+            user={user}
+            values={locationValues}
+            setValues={setLocationValues}
+            slot={locationSlot}
+            setSlot={setLocationSlot}
+            errors={errors}
+            setErrors={setErrors}
+            billing={locationBilling}
+            setBilling={setLocationBilling}
+            checkout
+          />
+        ),
+      },
       { title: "Shipping" },
       { title: "Payment" },
       { title: "Confirmation" },
--- src/components/settings/Location.js
+++ src/components/settings/Location.js
@@ -3,6 +3,8 @@ import Grid from '@material-ui/core/Grid'
 import axios from 'axios'
 import Chip from '@material-ui/core/Chip'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -18,7 +20,7 @@ import zipAdornment from '../../images/zip-adornment.svg'
 
 const useStyles = makeStyles(theme => ({
     icon: {
-        marginBottom: '3rem',
+        marginBottom: ({checkout }) => checkout ? '1rem' : '3rem',
         [theme.breakpoints.down('xs')]: {
           marginBottom: '1rem',
         },
@@ -41,8 +43,16 @@ const useStyles = makeStyles(theme => ({
     },
     slotContainer: {
         position: 'absolute',
-        bottom: 0,
+        bottom: ({ checkout }) => checkout ? -8 : 0,
     },
+    switchWrapper: {
+      marginRight: 4,
+    },
+    switchLabel: {
+      color: '#fff',
+      fontWeight: 600,
+    },
+    
 }))
 
 export default function Location({
@@ -55,8 +65,11 @@ export default function Location({
   setSlot,
   errors,
   setErrors,
+  checkout,
+  billing,
+  setBilling
 }) {
-  const classes = useStyles()
+  const classes = useStyles({ checkout })
   const [loading, setLoading] = useState(false)
   const { dispatchFeedback } = useContext(FeedbackContext)
 
@@ -83,11 +96,15 @@ export default function Location({
   }, [slot])
 
   useEffect(() => {
-    const changed = Object.keys(user.locations[slot]).some(
-      field => values[field] !== user.locations[slot][field]
-    )
+    if(!checkout) {
+      const changed = Object.keys(user.locations[slot]).some(
+        field => values[field] !== user.locations[slot][field]
+      )
+  
+      setChangesMade(changed)
+    }
 
-    setChangesMade(changed)
+ 
 
     if(values.zip.length === 5) {
       if(values.city) return
@@ -117,7 +134,7 @@ export default function Location({
       container
       direction="column"
       alignItems="center"
-      lg={6}
+      lg={checkout ? 12 : 6}
       xs={12}
       justifyContent="center"
       classes={{ root: classes.locationContainer }}
@@ -143,7 +160,7 @@ export default function Location({
           errors={errors}
           setErrors={setErrors}
           isWhite
-          disabled={!edit}
+          disabled={checkout ? false : !edit}
         />
       </Grid>
       <Grid item classes={{ root: classes.chipWrapper }}>
@@ -156,8 +173,24 @@ export default function Location({
         )}
         
       </Grid>
-      <Grid item container classes={{ root: classes.slotContainer }}>
-        <Slots slot={slot} setSlot={setSlot} />
+      <Grid item  container justifyContent='space-between' classes={{ root: classes.slotContainer }}>
+        <Slots slot={slot} setSlot={setSlot} checkout={checkout} />
+        {checkout && (
+          <Grid item>
+            <FormControlLabel
+            classes={{root: classes.switchWrapper, label: classes.switchLabel}}
+              label="Billing"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={billing}
+                  onChange={() => setBilling(!billing)}
+                  color="secondary"
+                />
+              }
+            />
+          </Grid>
+        )}
       </Grid>
     </Grid>
   )

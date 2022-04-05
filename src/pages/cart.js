5bf1b4512c8f3d4ce302bae1ea7e8e3e6df99151
Drilon <navycrack1@gmail.com>
2022-04-05 18:17:28 +0200

    cart responsive design

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -24,11 +24,11 @@ export default function Cart() {
     <Layout>
       <Grid container direction="column" alignItems="center" classes={{root: classes.cartContainer}}>
         <Grid item>
-          <Typography variant="h1">{user.username}'s Cart</Typography>
+          <Typography variant="h1" align='center'>{user.username}'s Cart</Typography>
         </Grid>
         <Grid item container>
             <CartItems />
-            <CheckoutPortal user={user} />
+            {/* <CheckoutPortal user={user} /> */}
         </Grid>
       </Grid>
     </Layout>

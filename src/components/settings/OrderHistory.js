25008a5b0a187b26b34d6426285ade52a2f628c9
drilon305 <drilonsulejmani305@gmail.com>
2022-05-14 23:16:04 +0200

    order history navigation

--- src/components/settings/OrderHistory.js
+++ src/components/settings/OrderHistory.js
@@ -3,8 +3,11 @@ import axios from 'axios'
 import Grid from '@material-ui/core/Grid'
 import Chip from '@material-ui/core/Chip'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
 import { DataGrid } from '@material-ui/data-grid'
 import { makeStyles } from '@material-ui/core/styles'
+
+import BackwardsIcon from '../../images/BackwardsOutline'
 
 import { UserContext } from '../../contexts'
 
@@ -15,6 +18,16 @@ const useStyles = makeStyles(theme => ({
     },
     chipLabel: {
         fontWeight: 600,
+        fontSize: '1rem'
+    },
+    header: {
+        height: '8rem',
+        width: '100%',
+        backgroundColor: theme.palette.secondary.main
+    },
+    icon: {
+        height: '4rem', 
+        width: '4rem'
     },
     "@global": {
         ".MuiDataGrid-root .MuiDataGrid-columnHeaderTitle": {
@@ -29,22 +42,27 @@ const useStyles = makeStyles(theme => ({
         ".MuiDataGrid-root .MuiDataGrid-columnHeader--moving": {
             "background-color": 'transparent'
         },
+        ".MuiDataGrid-root .MuiDataGrid-window": {
+            "overflow": "auto"
+        },
         ".MuiDataGrid-root .MuiDataGrid-cell": {
-            'white-space': 'pre-wrap',
-            'max-height': '100% !important',
-            'line-height': 'initial !important',
-            padding: '1rem',
-            'padding-right': 'calc(1rem + 26px)',
-            display: 'flex',
-            'justify-content': 'center',
-            'align-items': 'center',
-            'font-weight': 600,
+             padding: "1rem",
+            "padding-right": "calc(1rem + 26px)",
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            "font-weight": 600,
+            "border-bottom": "2px solid #fff",
+        },
+        ".MuiDataGrid-root .MuiDataGrid-cell--textLeft.MuiDataGrid-cell--withRenderer": {
+            'justify-content': 'center'
         },
         ".MuiDataGrid-root .MuiDataGrid-row": {
             'max-height': '100% !important',
+            
         },
-        ".MuiDataGrid-root .MuiDataGrid-footer": {
-            'margin-top': '-12rem'
+        ".MuiDataGrid-root .MuiDataGrid-footerContainer": {
+            "margin-top": "-11rem",
         },
         ".MuiTablePagination-caption": {
             color: '#fff',
@@ -52,47 +70,83 @@ const useStyles = makeStyles(theme => ({
         ".MuiSvgIcon-root": {
             fill: '#fff',
         },
+        ".MuiDataGrid-root .MuiDataGrid-columnHeaderWrapper": {
+            'background-color': theme.palette.secondary.main,
+            border: 'none'
+        },
+        ".MuiDataGrid-root": {
+            border: 'none'
+        },
     },
 }))
 
-export default function OrderHistory() {
-    const classes = useStyles()
-    const [orders, setOrders] = useState([])
-    const { user } = useContext(UserContext)
+export default function OrderHistory({ setSelectedSetting }) {
+  const classes = useStyles()
+  const [orders, setOrders] = useState([])
+  const { user } = useContext(UserContext)
 
-    useEffect(() => {
-        axios.get(process.env.GATSBY_STRAPI_URL + '/orders/history', {
-            headers: { Authorization: `Bearer ${user.jwt}`}
-        }).then(response => {
-            setOrders(response.data.orders)
-        }).catch(error => {
-            console.error(error)
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(process.env.GATSBY_STRAPI_URL + "/orders/history", {
+        headers: { Authorization: `Bearer ${user.jwt}` },
+      })
+      .then(response => {
+        setOrders(response.data.orders)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
 
-    const createData = data => data.map(item => ({
-        shipping: `${item.shippingInfo.name}\n${item.shippingAddress.street}\n${item.shippingAddress.city}, ${item.shippingAddress.state} ${item.shippingAddress.zip}`,
-        order: `#${item.id.slice(item.id.length - 10, item.id.length).toUpperCase()}`,
-        status: item.status,
-        date: `${item.createdAt.split('-')[1]}/${item.createdAt.split('-')[2].split('T')[0]}/${item.createdAt.split('-')[0]}`,
-        total: item.total,
-        id: item.id
+  const createData = data =>
+    data.map(item => ({
+      shipping: `${item.shippingInfo.name}`,
+      order: `#${item.id
+        .slice(item.id.length - 10, item.id.length)
+        .toUpperCase()}`,
+      status: item.status,
+      date: `${item.createdAt.split("-")[1]}/${
+        item.createdAt.split("-")[2].split("T")[0]
+      }/${item.createdAt.split("-")[0]}`,
+      total: item.total,
+      id: item.id,
     }))
 
-    const columns = [
-      { field: "shipping", headerName: 'Shipping', flex: 1, sortable: false },
-      { field: "order",  headerName: 'Order', flex: 1 },
-      { field: "status",  headerName: 'Status', flex: 1, renderCell: ({ value }) => <Chip label={value} classes={{label: classes.chipLabel}} />, },
-      { field: "date",  headerName: 'Date', flex: 1, type: 'date' },
-      { field: "total",  headerName: 'Total', flex: 1, renderCell: ({ value }) => <Chip label={`$${value}`} classes={{label: classes.chipLabel}} /> },
-      { field: '', flex: 1.5, sortable: false},
-    ]
+  const columns = [
+    { field: "shipping", headerName: "Shipping", flex: 1, sortable: false },
+    { field: "order", headerName: "Order", flex: 1 },
+    {
+      field: "status",
+      headerName: "Status",
+      flex: 1,
+      renderCell: ({ value }) => (
+        <Chip label={value} classes={{ label: classes.chipLabel }} />
+      ),
+    },
+    { field: "date", headerName: "Date", flex: 1, type: "date" },
+    {
+      field: "total",
+      headerName: "Total",
+      flex: 1,
+      renderCell: ({ value }) => (
+        <Chip label={`$${value}`} classes={{ label: classes.chipLabel }} />
+      ),
+    },
+    { field: "", flex: 1.5, sortable: false },
+  ]
 
-    const rows = createData(orders)
+  const rows = createData(orders)
 
-    return (
-        <Grid item classes={{root: classes.item}}>
-            <DataGrid rows={rows} columns={columns} pageSize={5} />
-        </Grid>
-    )
+  return (
+    <Grid item container classes={{ root: classes.item }}>
+      <Grid item classes={{ root: classes.header }}>
+        <IconButton onClick={() => setSelectedSetting(null)}>
+          <div className={classes.icon}>
+            <BackwardsIcon color="#fff" />
+          </div>
+        </IconButton>
+      </Grid>
+      <DataGrid rows={rows} columns={columns} pageSize={5} />
+    </Grid>
+  )
 }
